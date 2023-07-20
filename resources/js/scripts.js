72be30512c07ea314cb8c3760336a702e3eccc69
carloeusebi <carloeusebi@gmail.com>
2023-07-20 17:44:04 +0200

    Frontend can now save new tasks

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -5,9 +5,34 @@ const { createApp } = Vue;
 const app = createApp({
 	data() {
 		return {
-			helloWorld: 'hello world',
 			tasks: [],
+			newTask: '',
 		};
+	},
+	methods: {
+		handleTaskClick(id) {
+			console.log(this.tasks);
+			const taskToUpdate = this.tasks.find(task => task.id === id);
+
+			taskToUpdate.completed = !taskToUpdate.completed;
+		},
+		addTask() {
+			if (!this.newTask) return;
+
+			const params = {
+				task: this.newTask,
+			};
+
+			const config = {
+				headers: { 'Content-Type': 'multipart/form-data' },
+			};
+
+			axios.post(baseURL, params, config).then(({ data }) => {
+				this.tasks.push(data);
+			});
+
+			this.newTask = '';
+		},
 	},
 	mounted() {
 		axios.get(baseURL).then(({ data }) => {
